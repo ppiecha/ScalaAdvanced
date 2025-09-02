7099fb7013b97e16709d80561a925788f82b7fd3
Piotr Piecha <piotr.piecha@outlook.com>
2025-09-02 06:07:42 +0200

    + applicative

--- src/main/scala/applicative/ApplicativeList.scala
+++ src/main/scala/applicative/ApplicativeList.scala
@@ -1,7 +1,7 @@
 package applicative
 
 object ApplicativeList {
-  val applicativeList = new Applicative[List] {
+  val applicativeList: Applicative[List] = new Applicative[List] {
     override def pure[A](a: A): List[A] = List(a)
     override def ap[A, B](ff: List[A => B])(aa: List[A]): List[B] = (ff, aa) match {
       case (fh :: ft, h :: t) => (h :: t).map(fh) ++ ap(ft)(h :: t)
